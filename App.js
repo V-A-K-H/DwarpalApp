a27f60d5fbb3b8f3c7ce41e6ea65d31ef3f5f4e1
Harshit Roy <101564851+harshit-roy@users.noreply.github.com>
2023-10-10 21:21:22 +0530

    Minor fix

--- App.js
+++ App.js
@@ -11,149 +11,151 @@ import Home from "./assets/Screens/Home";
 import Info from "./assets/Screens/Info"
 import Maps from "./assets/Screens/Maps"
 import Todo from "./assets/Screens/Todo";
+import LoginPage from "./assets/Screens/LoginPage"
 const Tab = createBottomTabNavigator();
 export default function App() {
   return (
-    <NavigationContainer>
-      <Tab.Navigator
-        tabBarOptions={{ showLabel: false }}
-        screenOptions={{
-          tabBarStyle: {
-            backgroundColor: "white",
-            position: "absolute",
-            bottom: 20,
-            marginHorizontal: 20,
-            height: 60,
-            borderRadius: 10,
-            shadowColor: "#000",
-            shadowOpacity: 0.06,
-            shadowOffset: {
-              width: 8,
-              height: 8,
-            },
-            paddingHorizontal: 20,
-          },
-        }}
-      >
-        {
-          //Tab Screens
-        }
-        <Tab.Screen
-          name={"Home"}
-          component={HomeScreen}
-          options={{
-            headerShown:false,
-            tabBarIcon: ({ focused }) => (
-              <View
-                style={{
-                  position: "absolute",
-                }}
-              >
-                <FontAwesome5
-                  name="home"
-                  size={22}
-                  color={focused ? "black" : "grey"}
-                ></FontAwesome5>
-              </View>
-            ),
-          }}
-        ></Tab.Screen>
+    // <NavigationContainer>
+    //   <Tab.Navigator
+    //     tabBarOptions={{ showLabel: false }}
+    //     screenOptions={{
+    //       tabBarStyle: {
+    //         backgroundColor: "white",
+    //         position: "absolute",
+    //         bottom: 20,
+    //         marginHorizontal: 20,
+    //         height: 60,
+    //         borderRadius: 10,
+    //         shadowColor: "#000",
+    //         shadowOpacity: 0.06,
+    //         shadowOffset: {
+    //           width: 8,
+    //           height: 8,
+    //         },
+    //         paddingHorizontal: 20,
+    //       },
+    //     }}
+    //   >
+    //     {
+    //       //Tab Screens
+    //     }
+    //     <Tab.Screen
+    //       name={"Home"}
+    //       component={HomeScreen}
+    //       options={{
+    //         headerShown:false,
+    //         tabBarIcon: ({ focused }) => (
+    //           <View
+    //             style={{
+    //               position: "absolute",
+    //             }}
+    //           >
+    //             <FontAwesome5
+    //               name="home"
+    //               size={22}
+    //               color={focused ? "black" : "grey"}
+    //             ></FontAwesome5>
+    //           </View>
+    //         ),
+    //       }}
+    //     ></Tab.Screen>
 
-        <Tab.Screen
-          name={"Maps"}
-          component={MapsScreen}
-          options={{
-            headerShown:false,
-            tabBarIcon: ({ focused }) => (
-              <View
-                style={{
-                  position: "absolute",
-                }}
-              >
-                <FontAwesome5
-                  name="map-marked-alt"
-                  size={22}
-                  color={focused ? "black" : "grey"}
-                ></FontAwesome5>
-              </View>
-            ),
-          }}
-        ></Tab.Screen>
+    //     <Tab.Screen
+    //       name={"Maps"}
+    //       component={MapsScreen}
+    //       options={{
+    //         headerShown:false,
+    //         tabBarIcon: ({ focused }) => (
+    //           <View
+    //             style={{
+    //               position: "absolute",
+    //             }}
+    //           >
+    //             <FontAwesome5
+    //               name="map-marked-alt"
+    //               size={22}
+    //               color={focused ? "black" : "grey"}
+    //             ></FontAwesome5>
+    //           </View>
+    //         ),
+    //       }}
+    //     ></Tab.Screen>
 
-        <Tab.Screen
-          name={"camera"}
-          component={CameraScreen}
-          options={{
-            headerShown:false,
-            tabBarIcon: ({ focused }) => (
+    //     <Tab.Screen
+    //       name={"camera"}
+    //       component={CameraScreen}
+    //       options={{
+    //         headerShown:false,
+    //         tabBarIcon: ({ focused }) => (
               
-                <View
-                  style={{
-                    width: 60,
-                    height: 60,
-                    backgroundColor:"red",
-                    borderRadius: "50%",
-                    justifyContent: "center",
-                    alignItems: "center",
-                    marginBottom: 50
-                  }}
-                >
-                  <Image
-                    source={Camera}
-                    style={{
-                      width: 26,
-                      height: 26,
-                      tintColor: "white",
-                    }}
-                  ></Image>
-                </View>
-            ),
-          }}
-        ></Tab.Screen>
+    //             <View
+    //               style={{
+    //                 width: 60,
+    //                 height: 60,
+    //                 backgroundColor:"red",
+    //                 borderRadius: "50%",
+    //                 justifyContent: "center",
+    //                 alignItems: "center",
+    //                 marginBottom: 50
+    //               }}
+    //             >
+    //               <Image
+    //                 source={Camera}
+    //                 style={{
+    //                   width: 26,
+    //                   height: 26,
+    //                   tintColor: "white",
+    //                 }}
+    //               ></Image>
+    //             </View>
+    //         ),
+    //       }}
+    //     ></Tab.Screen>
 
-        <Tab.Screen
-          name={"Info"}
-          component={InfoScreen}
-          options={{
-            headerShown:false,
-            tabBarIcon: ({ focused }) => (
-              <View
-                style={{
-                  position: "absolute",
-                }}
-              >
-                <FontAwesome5
-                  name="id-card-alt"
-                  size={22}
-                  color={focused ? "black" : "grey"}
-                ></FontAwesome5>
-              </View>
-            ),
-          }}
-        ></Tab.Screen>
+    //     <Tab.Screen
+    //       name={"Info"}
+    //       component={InfoScreen}
+    //       options={{
+    //         headerShown:false,
+    //         tabBarIcon: ({ focused }) => (
+    //           <View
+    //             style={{
+    //               position: "absolute",
+    //             }}
+    //           >
+    //             <FontAwesome5
+    //               name="id-card-alt"
+    //               size={22}
+    //               color={focused ? "black" : "grey"}
+    //             ></FontAwesome5>
+    //           </View>
+    //         ),
+    //       }}
+    //     ></Tab.Screen>
 
-        <Tab.Screen
-          name={"ToDo"}
-          component={TodoScreen}
-          options={{
-            headerShown:false,
-            tabBarIcon: ({ focused }) => (
-              <View
-                style={{
-                  position: "absolute",
-                }}
-              >
-                <FontAwesome5
-                  name="bars"
-                  size={22}
-                  color={focused ? "black" : "grey"}
-                ></FontAwesome5>
-              </View>
-            ),
-          }}
-        ></Tab.Screen>
-      </Tab.Navigator>
-    </NavigationContainer>
+    //     <Tab.Screen
+    //       name={"ToDo"}
+    //       component={TodoScreen}
+    //       options={{
+    //         headerShown:false,
+    //         tabBarIcon: ({ focused }) => (
+    //           <View
+    //             style={{
+    //               position: "absolute",
+    //             }}
+    //           >
+    //             <FontAwesome5
+    //               name="bars"
+    //               size={22}
+    //               color={focused ? "black" : "grey"}
+    //             ></FontAwesome5>
+    //           </View>
+    //         ),
+    //       }}
+    //     ></Tab.Screen>
+    //   </Tab.Navigator>
+    // </NavigationContainer>
+    <LoginPage/>
   );
 }
 
